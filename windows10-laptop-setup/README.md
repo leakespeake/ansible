fd8b553cd42deb531d17eee05cb0ebde2d890ee3
leakespeake <barryleake@hotmail.com>
2021-02-07 06:43:59 +0000

    win10 setup - initial

---
+++ windows10-laptop-setup/README.md
@@ -0,0 +1,8 @@
+# Windows 10 Laptop Setup
+
+The purpose of this Ansible project is to automate (as much as possible) the software installation, updates and configuration for my Windows 10 development laptop and covers the following 2 local environments;
+
+- Windows 10 Home (OS)
+- Ubuntu 20.04 (WSL2)
+
+This will become useful to re-create my development environment in the event that I switch to or use additional hardware. 
---
+++ windows10-laptop-setup/win10-host-setup.ps1
@@ -0,0 +1,14 @@
+<#
+Setup WinRM for Ansible communications
+#>
+$url = "https://raw.githubusercontent.com/ansible/ansible/devel/examples/scripts/ConfigureRemotingForAnsible.ps1"
+$file = "$env:temp\ConfigureRemotingForAnsible.ps1"
+(New-Object -TypeName System.Net.WebClient).DownloadFile($url, $file)
+powershell.exe -ExecutionPolicy ByPass -File $file
+
+<#
+Enable WSL2 and Virtual Machine Platform then reboot
+#>
+dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
+dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
+Restart-Computer
