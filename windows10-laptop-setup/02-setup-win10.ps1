1e1acd65780c519428eaf80e37105c7663cebfa0
leakespeake <barryleake@hotmail.com>
2021-08-23 08:24:19 +0100

    amendments since testing

--- windows10-laptop-setup/02-setup-win10.ps1
+++ windows10-laptop-setup/02-setup-win10.ps1
@@ -19,14 +19,14 @@ exit
 # Source location of WSL2 kernel update
 $source = 'https://wslstorestorage.blob.core.windows.net/wslblob/wsl_update_x64.msi'
 # Destination to save the WSL2 kernel update
-$destination = 'C:\Users\barry\Downloads\wsl_update_x64.msi'
+$destination = "C:\Users\$env:USERNAME\Downloads\wsl_update_x64.msi"
 # Download the WSL2 kernel update
 Invoke-WebRequest -Uri $source -OutFile $destination
 
 
 # Install the WSL2 kernel update - check if still required for your OS version via: Get-ComputerInfo OsName,OsVersion,OsBuildNumber,WindowsVersion
-cd C:\Users\barry\Downloads
-msiexec /package wsl_upate_x64.msi /quiet
+cd C:\Users\$env:USERNAME\Downloads
+msiexec /package wsl_update_x64.msi /quiet
 
 
 # Set the WSL default version
